72deb31927071a301e072afbeed0257142167a5b
PleshkovaAnastasia1992 <nastya.tsvetkova@rambler.ru>
2024-06-13 15:02:14 +0500

    Add new homepage

--- mailing/views.py
+++ mailing/views.py
@@ -1,13 +1,32 @@
+import random
+
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView, DetailView, ListView, UpdateView, CreateView, TemplateView
 
+from blog.models import BlogPost
+from client.models import Client
 from mailing.models import MailingSettings, MailingMessage, MailingStatus
 
 
 class HomeTemplateView(TemplateView):
     """Контроллер для главной страницы"""
     template_name = 'mailing/home.html'
+
+    def get_context_data(self, **kwargs):
+        mailing_count = MailingMessage.objects.count()
+        is_active_count = MailingSettings.objects.filter(setting_status='Started').count()
+        clients_count = Client.objects.distinct('email').count()
+        blog_list = list(BlogPost.objects.all())
+        random.shuffle(blog_list)
+        random_blog_list = blog_list[:3]
+        context_data = {
+            'mailing_count': mailing_count,
+            'is_active': is_active_count,
+            'clients_count': clients_count,
+            'random_blog_list': random_blog_list,
+        }
+        return context_data
 
 
 class MailingMessageCreateView(CreateView):
